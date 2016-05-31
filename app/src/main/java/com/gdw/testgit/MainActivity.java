457978e613878e15de0de8aa3051562b253e1245
steven <1968965852@qq.com>
2016-05-31 17:16:50 +0800

    git版本控制第二版

--- app/src/main/java/com/gdw/testgit/MainActivity.java
+++ app/src/main/java/com/gdw/testgit/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "git版本控制", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "git版本控制，第二次更新", Toast.LENGTH_SHORT).show();
+
 
     }
 }
